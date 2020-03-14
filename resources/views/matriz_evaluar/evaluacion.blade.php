c0fcd8593b0ee9894db384b36312cc478df44203
Carlos Andres Loja <cal-107@hotmail.com>
2020-03-13 23:02:09 -0500

    Actualizar

--- resources/views/matriz_evaluar/evaluacion.blade.php
+++ resources/views/matriz_evaluar/evaluacion.blade.php
@@ -4,15 +4,18 @@
         .nav-tabs > li.active > a {
             color: #444 !important;
         }
-        .nav-tabs-custom  > .nav-tabs > li > a {
+
+        .nav-tabs-custom > .nav-tabs > li > a {
             color: white;
             font-weight: bold;
         }
-        .nav-tabs-custom  > .nav-tabs > li > a:hover {
+
+        .nav-tabs-custom > .nav-tabs > li > a:hover {
             color: black;
             /*border-color: transparent;
             background: transparent;*/
         }
+
         /*.nav-tabs > li > a {
             margin-right: 0px;
             line-height: 1.42857143;
@@ -49,20 +52,20 @@
 
                     <div class="nav-tabs-custom">
                         {{--PESTAÑAS DE CRITERIOS--}}
-                            <ul class="nav nav-tabs">
-                                @foreach($criterios as $criterio)
-                                    <li class="bg-primary {{$tabActiva == $criterio->criterio_id ? 'active':''}}">
-                                        <a href="#tab_{{$criterio->criterio_id}}" data-toggle="tab"
-                                           aria-expanded="true">{{$criterio->estrucEvide->estructuraIndicadore->estructuraCriterio->criterio->nombre}}</a>
-                                    </li>
-                                @endforeach
-                            </ul>
+                        <ul class="nav nav-tabs">
+                            @foreach($criterios as $criterio)
+                                <li class="bg-primary {{$tabActiva == $criterio->criterio_id ? 'active':''}}">
+                                    <a href="#tab_{{$criterio->criterio_id}}" data-toggle="tab"
+                                       aria-expanded="true">{{$criterio->estrucEvide->estructuraIndicadore->estructuraCriterio->criterio->nombre}}</a>
+                                </li>
+                            @endforeach
+                        </ul>
                         {{--CONTENIDO DE LAS PESTAÑAS--}}
                         <div class="tab-content">
                             @foreach($criterios as $criterio)
                                 <div class="tab-pane {{$tabActiva == $criterio->criterio_id ? 'active':''}}"
                                      id="tab_{{$criterio->criterio_id}}">
-                                    <table class="table table-responsive table-striped table-bordered table-hover table-checkable datatable tblElementosEvaluar" >
+                                    <table class="table table-responsive table-striped table-bordered table-hover table-checkable datatable tblElementosEvaluar">
                                         <thead>
                                         <tr>
                                             <th width="175">Indicador</th>
@@ -75,10 +78,12 @@
                                         @foreach($elementos as $elemento)
                                             @if ($elemento->criterio_id == $criterio->criterio_id)
                                                 <tr>
-                                                    <td>{{$elemento->estrucEvide->estructuraIndicadore->indicador->nombre}}</td>
+                                                    <td>{{$elemento->estrucEvide->estructuraIndicadore->indicador -> nombre}}</td>
                                                     <td>{{$elemento->estrucEvide->evidencia->nombre}}</td>
                                                     <td>{{$elemento->elemento->nombre}}</td>
-                                                    <td><button class="btn btn-primary" type="button">Evaluar</button></td>
+                                                    <td>
+                                                        <button class="btn btn-primary" type="button">Evaluar</button>
+                                                    </td>
                                                 </tr>
                                             @endif
                                         @endforeach
